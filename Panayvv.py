8090177807fc3d325bd2b4d2cd377c26b7086d95
Panayvv <GFGexample@gmail.orgg>
2024-12-10 14:10:23 +0300

    2 test

--- Panayvv.py
+++ Panayvv.py
@@ -4,21 +4,21 @@ a=int(input())
 b=int(input())
 print(a+b)
 
-# # masala8
-# n=int(input('N='))
-# while n!=10:
-#     if  n!=10:
-#         print("Notogri")
-#     else:
-#         print("Togri")
-#         break
+# masala8
+n=int(input('N='))
+while n!=10:
+    if  n!=10:
+        print("Notogri")
+    else:
+        print("Togri")
+        break
 
-# # masala10
-# n=int(input('N='))
-# for i in range(1,n+1):
-#     i*=2
-#     print(i)
-#     if i==n:
-#         break
-# y=n+i
-# print(y)
+# masala10
+n=int(input('N='))
+for i in range(1,n+1):
+    i*=2
+    print(i)
+    if i==n:
+        break
+y=n+i
+print(y)
