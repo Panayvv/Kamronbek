c82cb286f428294c4300dd8dbfdb05f00e6e875b
Panayvv <GFGexample@gmail.orgg>
2024-12-10 15:33:17 +0300

    test uchun yozdm

--- tuple.py
+++ tuple.py
@@ -1,17 +1,17 @@
-s=(2,4,5,4)
-index=s.index(4)
-cout=s.count(4)
-print(index,cout)
-print(s,type(s))
-
-def test (a,b):
-    c=a+b
-    d=a-b
-    return a,b
-
-s1={5,7,6,4,6,4,4.5,'Salom'}
-s2={5,7,5,11.8,5,6 ,47}
-print(s1-s2)
-print(s2-s1)
-print(s1&s2)
-print(s1^s2)
+# s=(2,4,5,4)
+# index=s.index(4)
+# cout=s.count(4)
+# print(index,cout)
+# print(s,type(s))
+#
+# def test (a,b):
+#     c=a+b
+#     d=a-b
+#     return a,b
+#
+# s1={5,7,6,4,6,4,4.5,'Salom'}
+# s2={5,7,5,11.8,5,6 ,47}
+# print(s1-s2)
+# print(s2-s1)
+# print(s1&s2)
+# print(s1^s2)
