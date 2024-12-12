cc476773825abf93b4382437ce578a801e5a3fd2
Panayvv <GFGexample@gmail.orgg>
2024-12-12 14:22:18 +0300

    2 test

---
+++ Robocontest.py
@@ -0,0 +1,39 @@
+# a=str(input())
+# b=str(input())
+# print(a+b)
+
+# a, b, c = map(int, input().split())
+# if a < b or b > c or c < a:
+#     d = b - c
+#     print(d)
+# elif a > b or b > c or c > b:
+#     e = a - b
+#     print(e)
+# elif a < c or b > a or c > a:
+#     f = c - a
+#     print(f)
+
+# n=int(input())
+# for i in range(1,11):
+#     c=i == range(2,8)
+# print("econom 105 k")
+
+
+# n=int(input())
+# for i in range(1,n):
+#     c=i**3
+#     if i%2==1:
+#         print("Qiziqarli")
+
+# if "11111" in input():
+#     print("YES")
+# else:
+#     print("No")
+
+# a=int(input())
+# b=int(input())
+# counter=0
+# for i in range(a,b):
+#     counter+=1
+# print(counter)
+
